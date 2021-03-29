2da360e49b97284e2657a5d33ec05739730410d7
Jeet009 <jeetmukherjee009@gmail.com>
2021-03-30 00:07:39 +0530

    initial commit

---
+++ routes/articles.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Article = require("../models/articles");
+
+// Fetching All The Articles
+router.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.send(articles);
+});
+
+// Add Data To Article Database
+router.post("/add", async (req, res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    article = await article.save();
+    res.send("Article Added" + article.id);
+  } catch (e) {
+    console.log("Couldn't Add", e);
+  }
+});
+
+// Getting Details Of A Perticular Article
+router.get("/:slug", async (req, res) => {
+  const articleDetail = await Article.findOne({ slug: req.params.slug });
+  res.send(articleDetail);
+});
+
+//Deleting Articles
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const articleRouter = require("./routes/articles");
+const bodyParser = require("body-parser");
+const app = express();
+
+//Connecting To Database
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Setup Point For View (Incase of not using API)
+app.set("view engine", "ejs");
+
+// Url Encoder
+app.use(express.urlencoded({ extended: false }));
+
+// Middleware
+app.use(bodyParser.json());
+
+// Intializing Route - { Using Middleware }
+app.use("/articles", articleRouter);
+
+//Listening to Port 5000
+app.listen(5000);
